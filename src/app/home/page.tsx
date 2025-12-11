dbf648eaedd29141ab410f799c1486ffaa65732c
Marsad Ghanvi <marsad11223@gmail.com>
2025-12-11 19:40:59 +0500

    unwanted components removed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,14 @@
-// import { Box } from "@mui/material";
+import { Box } from "@mui/material";
 import HeroHome from "./_sections/HeroHome";
 import CoreValues from "./_sections/CoreValues";
-// import AboutUs from "./_sections/AboutUs";
-import ClientsExperiences from "./_sections/ClientsExperiences";
-import NewsArticles from "@/_components/NewsArticles";
-// import NewsUpdates from "./_sections/NewsUpdates";
 import ContactUs from "@/_components/ContactUs";
 import Footer from "@/_components/Footer";
-import CommitedToJustice from "./_sections/CommitedToJustice";
-import LegalServicesTabs from "./_sections/LegalServicesTabs";
 import InfoSection from "@/_components/InfoSection";
 import pngs from "@/_assets/webp";
 import WhatWeOffer from "./_sections/WhatWeOffer";
 import RealCasesRealResults from "./_sections/RealCasesRealResults";
 import InsightsAndLegalGuidance from "./_sections/InsightsAndLegalGuidance";
 import WhatOurCustomersSay from "./_sections/WhatOurCustomersSay";
-import { Box } from "@mui/material";
-// import { Box } from "@mui/material";
 
 export default function HomePage() {
   return (
@@ -50,18 +42,6 @@ export default function HomePage() {
       <InsightsAndLegalGuidance />
       <WhatOurCustomersSay />
       <ContactUs />
-      <Footer />
-
-      {/* above this   */}
-      <CoreValues />
-      {/* <AboutUs /> */}
-      <CommitedToJustice />
-      <LegalServicesTabs />
-      <ClientsExperiences />
-      <NewsArticles />
-      {/* <NewsUpdates /> */}
-      <ContactUs />
-      {/* </Box> */}
       <Footer />
     </>
   );
